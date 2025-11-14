2acd7a478800a9e100a74f8423176cb575ecf6cd
norsultani <norsultani@gmail.com>
2025-11-14 17:42:59 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -2,13 +2,18 @@ def split_before_each_uppercases(formula):
     start = 0
     split_formula = []
 
+    if formula == "":
+      return split_formula
+      
+
     
     for end in range(1,len(formula)):
         if formula[end].isupper():
             split_formula.append(formula[start:end])
             start = end
+        
+   
 
-    
     split_formula.append(formula[start:])
 
     return split_formula
